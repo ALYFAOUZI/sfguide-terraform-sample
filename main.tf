c1806aba451c84c230e131d6b8ac855837a8c3b6
Faouziali <aly_faouzi@hotmail.com>
2024-02-15 14:40:45 -0500

    Add Database and Warehouse

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    snowflake = {
+      source  = "Snowflake-Labs/snowflake"
+      version = "~> 0.76"
+
+    }
+  }
+}
+
+provider "snowflake" {
+  private_key_path = "~/.ssh/snowflake_tf_snow_key.p8"
+  role = "SYSADMIN"
+}
+
+resource "snowflake_database" "db" {
+  name = "TF_DEMO"
+}
+
+resource "snowflake_warehouse" "warehouse" {
+  name           = "TF_DEMO"
+  warehouse_size = "large"
+  auto_suspend   = 60
+}
